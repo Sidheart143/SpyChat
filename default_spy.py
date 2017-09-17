e958ca7887e71c523a610356ec6a711251bf5a0e
Sidheart143 <siddharthjain262122@gmail.com>
2017-09-17 23:54:51 +0530

    project complete

---
+++ default_spy.py
@@ -0,0 +1,7 @@
+spy = {
+    'name': 'Peter',
+    'salutation': 'Parkar',
+    'age': 23,
+    'rating': 5,
+    'is_online': True
+}
---
+++ globals.py
@@ -0,0 +1,3 @@
+current_status_message = None
+ss = ["cool","m online","meet u soon"]
+friends = []
